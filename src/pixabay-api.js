98ab2a7ad80447d708240b3e95e23896a1fc0fae
Pauligrammer <150374386+Pauligrammer@users.noreply.github.com>
2024-05-10 00:26:49 +0200

    css

--- src/pixabay-api.js
+++ src/pixabay-api.js
@@ -22,20 +22,20 @@ export async function pixabayImg (searchQuery, page) {
 export function renderPhotos(images) {
     const photoCard = images
         .map(image => {
-            return `<div class="photo-card"><a href="${image.largeImageURL}"><img src="${image.webformatURL}" alt="${image.tags}" loading="lazy"/></a><div class="info"><p class="info-item">
-                <b>Likes</b>
+            return `<div class="photo-card"><a href="${image.largeImageURL}" class="link"><img src="${image.webformatURL}" alt="${image.tags}" loading="lazy"/></a><div class="info overlay"><p class="info-item">
+                <b>Likes:</b>
                 ${image.likes}
                 </p>
                 <p class="info-item">
-                <b>Views</b>
+                <b>Views:</b>
                 ${image.views}
                 </p>
                 <p class="info-item">
-                <b>Comments</b>
+                <b>Comments:</b>
                 ${image.comments}
                 </p>
                 <p class="info-item">
-                <b>Downloads</b>
+                <b>Downloads:</b>
                 ${image.downloads}
                 </p></div></div>`;
         })
